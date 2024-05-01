ad34a12a1ceaa22daa7b7f852e1da2af7ead83fd
Daniel Winship <Danielwinship@users.noreply.github.com>
2024-05-01 11:35:55 -0400

    Fixed to work with website changes

--- web.py
+++ web.py
@@ -6,23 +6,28 @@ from email.message import EmailMessage
 results = requests.get("https://www.thefreshmarket.com/specials/little-big-meal")
 results.encoding = "utf-8"
 cleanup = bs4.BeautifulSoup(results.text, 'html.parser')
-meal_elements = cleanup.find_all("div", class_="lbm-card")
+meal_elements = cleanup.select('div[class^="ItemGrid_TwoByTwoMd__"]')
   
 # creating list       
 global list 
 list = [] 
 
 for meal_element in meal_elements:
-  title_element = meal_element.find("h3", class_="lbm-card__title")
-  subtitle_element = meal_element.find("p", class_="lbm-card__subtitle")
-  available_element = meal_element.find(class_="lbm-card__tag")
-  mealLink = "https://www.thefreshmarket.com"+meal_element.contents[0].contents[0].attrs['href']
-  list.append(available_element.text)
-  list.append("Meal: " + title_element.text)
-  list.append("Description: " + subtitle_element.text)
+  title_element = meal_element.select('h3[class^="LBMCard_CardTitle_"]')
+  subtitle_element = meal_element.select('p[class^="LBMCard_CardSubtitle__"]')
+  available_element = meal_element.select('div[class^="LBMCard_CardTag__"]')
+  # Split the string into words
+  words = title_element[0].text
+  wordsLower = str(words.lower())
+  cleaned_link = wordsLower.replace(" ", "-")
+  mealLink = "https://www.thefreshmarket.com/specials/meals/little-big-meals/" + str(cleaned_link)
+
+  list.append(available_element[0].text)
+  list.append("Meal: " + title_element[0].text)
+  list.append("Description: " + subtitle_element[0].text)
   list.append("Link to meal: " + mealLink)
   list.append("\r\n")
-
+    
 port = 465  # For SSL
 password = "<password>"
 context = ssl.create_default_context()
